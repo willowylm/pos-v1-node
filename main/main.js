66c6b181a16d9281399b779db3e2c62b30e3ac6a
Liuming Yang <ylmap@126.com>
2018-07-29 11:42:53 +0800

    show goods imformation

--- main/main.js
+++ main/main.js
@@ -1,5 +1,6 @@
 
 // import (./main/database.js)
+const { loadAllItems, loadPromotions } = require("./datbase");
 
 module.exports = function printInventory(inputs) {
 
@@ -8,75 +9,31 @@ module.exports = function printInventory(inputs) {
     //Task2: 显示赠品信息
     //Task3: 显示总钱
 
-    var GoodsImformation = 
-        [{
-            barcode: 'ITEM000000',
-            name: '可口可乐',
-            unit: '瓶',
-            price: 3.00
-        },
-        {
-            barcode: 'ITEM000001',
-            name: '雪碧',
-            unit: '瓶',
-            price: 3.00
-        },
-        {
-            barcode: 'ITEM000002',
-            name: '苹果',
-            unit: '斤',
-            price: 5.50
-        },
-        {
-            barcode: 'ITEM000003',
-            name: '荔枝',
-            unit: '斤',
-            price: 15.00
-        },
-        {
-            barcode: 'ITEM000004',
-            name: '电池',
-            unit: '个',
-            price: 2.00
-        },
-        {
-            barcode: 'ITEM000005',
-            name: '方便面',
-            unit: '袋',
-            price: 4.50
-        }];
 
-        var GoodList=new Map();
 
-   for(var i=0;i<inputs.length;i++)
-   {
-       for(var j=0;j<GoodsImformation.length;j++)
-       {
-           if(inputs[i]==GoodsImformation[j].barcode)
-           {
-               {
-                   for(var key in GoodList)
-                   {
-                       if(key!=undefined)
-                       {
-                           GoodList.get(GoodsImformation[j].name)++;
-                       }
-                       else
-                       {
-                        GoodList.set(GoodsImformation[j].name,1);
 
-                       }
-                   }
-               }
-           
-            console.log('名称：'+GoodsImformation[j].name+'数量：'+GoodList.get(GoodsImformation[j].name)); 
-           }
-              
-       }
-   }
+    var goodList = new Map();
+
+    inputs.forEach(element => {
+        let splitAllTtems = element.split('-');
+        if(splitAllTtems.length == 1) {
+            if(good_list.get(splitAllTtems[0]) == undefined) {
+                good_list.set(splitAllTtems[0], 1);
+            } else{
+                good_list.set(splitAllTtems[0], good_list.get(splitAllTtems[0]) + 1);
+            }
+        } else if(splitAllTtems.length == 2) {
+            if(good_list.get(splitAllTtems[0]) == undefined) {
+              good_list.set(splitAllTtems[0], splitAllTtems[1]);
+           } else{
+              good_list.set(splitAllTtems[0], good_list.get(splitAllTtems[0]) + splitAllTtems[1]);
+          }
+        }
+    });
+
     
 
 
 
-    return 'Hello World!';
+    // return 'Hello World!';
 };
